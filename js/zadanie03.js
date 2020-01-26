d94022224d6eacf58c6863a309ff9bd2b650e2b0
Podgórski Oskar <oskar.podgorski@microsoft.wsei.edu.pl>
2020-01-26 15:46:08 +0100

    Zadanie03 Egzamin

--- js/zadanie03.js
+++ js/zadanie03.js
@@ -1,9 +1,9 @@
 function bigestSumOfTwoElements(array) {
 
-    if(array.length == 0) {
+    if (array.length == 0) {
         return false;
     }
-    else if(array.length == 1) {
+    else if (array.length == 1) {
         return console.log(array[0]);
     }
     else {
@@ -13,7 +13,7 @@ function bigestSumOfTwoElements(array) {
     }
 }
 
-console.log(bigestSumOfTwoElements([1,2,3,4]));
+console.log(bigestSumOfTwoElements([1, 2, 3, 4]));
 console.log(bigestSumOfTwoElements([]));
 console.log(bigestSumOfTwoElements([76]));
-console.log(bigestSumOfTwoElements([23,45,17,12]));
+console.log(bigestSumOfTwoElements([23, 45, 17, 12]));
